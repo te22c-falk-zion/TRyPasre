2f92f25e951305dc5776ed38f1571382f9909165
Zion Falk <zion.falk@elev.ga.ntig.se>
2024-05-22 23:05:46 +0200

    first commit

---
+++ TRyPasre/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// init
+string readthis;
+
+﻿Console.WriteLine("This is a new program that will \npredict the number you type in.");
+readthis = Console.ReadLine();
+
+// Tryparse
+while (!int.TryParse(readthis, out int value))
+{
+    Console.WriteLine("Are you making fun of me? huh? \ntry again and put down a number. \njerk");
+    readthis = Console.ReadLine();
+}
+
+// success
+Console.WriteLine($"{readthis}, cool huh? yeah i know. im awesome..\n\n\nheh..");
+Console.ReadLine();
